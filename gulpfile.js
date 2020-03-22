99ebd5d5a3ce7b2e5ece3d2b888043ad4b2cbf9f
xushengni <xushengni@tencent.com>
2020-03-22 21:33:49 +0800

    feat: 增加监听功能

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,36 @@ var br = require('browser-sync');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 
+var option='';
+
 gulp.task('build:html',function(cb){
-  gulp.src('src/**/*.html')
+  gulp.src('src/**/*.html',option)
   .pipe(gulp.dest('dist'))
-  .pipe(br.reload({stream: true}));
   cb();
 });
 
 gulp.task('build:style',function(cb){
-  gulp.src('src/**/*.less')
+  gulp.src('src/**/*.less',option)
   .pipe(sourcemaps.init())
   .pipe(less())
   .pipe(sourcemaps.write())
   .pipe(gulp.dest('dist'))
-  .pipe(br.reload({stream: true}));
   cb();
 });
 
-gulp.task('build',function(cb){
+gulp.task('build',function(){
   br.create().init({
-    server: {baseDir: './dist'},
+    server: {
+      baseDir: './dist'
+    },
+    files: '**',
     open: false,
     https: true,
     port: 8089
   });
-  cb();
 });
 
+
+gulp.watch('src/**/*.less',gulp.series('build:style'));
 gulp.task('default',gulp.series('build:html','build:style','build'));
+
